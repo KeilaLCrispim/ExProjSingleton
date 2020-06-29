9d6371c900493a53a5c385b8b296fc01d9f1dcc3
Keila Regina <kreginals@yahoo.com.br>
2020-06-29 20:07:56 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProjetoSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            singletonClass singleton = singletonClass.GetInstance;
+
+            Console.WriteLine(singleton.numero);
+
+            singleton.numero = 20;
+
+            singletonClass singleton2 = singletonClass.GetInstance;
+
+            Console.WriteLine(singleton2.numero);
+
+            singleton2.numero += 50;
+
+            Console.WriteLine(singleton.numero);
+
+            Console.Read();
+        }
+    }
+}
---
+++ singletonClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoSingleton
+{
+    class singletonClass
+    {
+        private static singletonClass singletonObject;
+
+        public int numero = 12;
+
+        private singletonClass()
+        {
+
+        }
+
+        public static singletonClass GetInstance 
+        {
+            get
+            {
+                if (singletonObject == null)
+                {
+                    singletonObject = new singletonClass();
+                }
+                return singletonObject;
+            }
+        }
+    }
+}
